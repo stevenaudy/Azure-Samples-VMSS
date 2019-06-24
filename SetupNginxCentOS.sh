0404b7fd64fee1aafbf5651d8b908c3eef8795ff
stevenaudy <49136081+stevenaudy@users.noreply.github.com>
2019-06-24 17:17:52 +0700

    Update SetupNginxCentOS.sh

--- SetupNginxCentOS.sh
+++ SetupNginxCentOS.sh
@@ -17,4 +17,4 @@ sudo systemctl start nginx
 # sudo firewall-cmd --reload
 
 # Set the home page.
-echo "<html><body><h2>Welcome to Azure! My name is $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
+# echo "<html><body><h2>Welcome to Azure! My name is $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
