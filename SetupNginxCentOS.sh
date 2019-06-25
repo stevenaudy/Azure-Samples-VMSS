1318c7718116ea60b1f1eba0bec7f321e243b512
stevenaudy <49136081+stevenaudy@users.noreply.github.com>
2019-06-25 14:51:57 +0700

    Update SetupNginxCentOS.sh

--- SetupNginxCentOS.sh
+++ SetupNginxCentOS.sh
@@ -2,11 +2,11 @@
 
 # Add Repository Nginx
 # Update system software
-yum -y update
-yum install epel-release
+sudo yum -y update
+sudo yum install epel-release
 # Install Nginx.
-yum install nginx
+sudo yum install nginx
 
 # Start Nginx
-systemctl enable nginx
-systemctl start nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
