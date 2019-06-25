0aed7121ba3bc50ba08191ea12e31e8826bc7a23
stevenaudy <49136081+stevenaudy@users.noreply.github.com>
2019-06-25 15:13:31 +0700

    Update SetupNginxCentOS.sh

--- SetupNginxCentOS.sh
+++ SetupNginxCentOS.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-# Add Repository Nginx
 # Update system software
 sudo yum -y update
-sudo yum install epel-release
+
+# Add Repository Nginx
+sudo yum -y install epel-release
+
 # Install Nginx.
-sudo yum install nginx
+sudo yum -y install nginx
 
 # Start Nginx
 sudo systemctl enable nginx
 sudo systemctl start nginx
+sudo systemctl enable firewalld
+sudo systemctl start firewalld
+firewall-cmd --zone=public --permanent --add-service=http
+firewall-cmd --zone=public --permanent --add-service=https
+firewall-cmd --reload
