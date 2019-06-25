c5cea6528b9e0129797bf51649347e384da2d956
stevenaudy <49136081+stevenaudy@users.noreply.github.com>
2019-06-25 17:49:41 +0700

    Update SetupNginxCentOS.sh

--- SetupNginxCentOS.sh
+++ SetupNginxCentOS.sh
@@ -18,7 +18,7 @@ sudo firewall-cmd --zone=public --permanent --add-service=http
 sudo firewall-cmd --zone=public --permanent --add-service=https
 sudo firewall-cmd --reload
 
-# Setup Laravel
+## Setup Laravel
 sudo rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
 sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 
@@ -27,3 +27,17 @@ sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 sudo yum install -y php71w php71w-curl php71w-common php71w-cli php71w-mysql php71w-mbstring php71w-fpm php71w-xml php71w-pdo php71w-zip
 sudo systemctl enable php-fpm
 sudo systemctl start php-fpm
+
+# install composer (Laravel Dependencies)
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/bin --filename=composer
+
+# install git
+sudo yum -y install git
+
+# install laravel
+## https://www.hugeserver.com/kb/install-laravel-centos-nginx/ (pakai ini lebih simpel)
+## https://tecadmin.net/install-laravel-framework-on-centos/
+## https://www.howtoforge.com/tutorial/how-to-install-laravel-5x-with-nginx-and-php-fpm-7-on-centos-7/
+sudo mkdir -p /var/www
+cd /var/www
+sudo git clone https://github.com/laravel/laravel.git
