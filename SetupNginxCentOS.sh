a642e9bc8b4f9f5fb6d994817857a85894dcad32
stevenaudy <49136081+stevenaudy@users.noreply.github.com>
2019-06-27 17:38:08 +0700

    Update SetupNginxCentOS.sh

--- SetupNginxCentOS.sh
+++ SetupNginxCentOS.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Update system software
-sudo yum -y update
-
 # Add Repository Nginx
 sudo yum -y install epel-release
 
@@ -10,21 +7,13 @@ sudo yum -y install epel-release
 sudo yum -y install nginx
 
 # Start Nginx
-sudo systemctl enable nginx
 sudo systemctl start nginx
-sudo systemctl enable firewalld
-sudo systemctl start firewalld
-sudo firewall-cmd --zone=public --permanent --add-service=http
-sudo firewall-cmd --zone=public --permanent --add-service=https
-sudo firewall-cmd --reload
+sudo systemctl enable nginx
 
-## Setup Laravel
-sudo rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
-sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
-
-# install php
+## Install PHP-FPM 7.1 for Nginx
 sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-sudo yum install -y php71w php71w-curl php71w-common php71w-cli php71w-mysql php71w-mbstring php71w-fpm php71w-xml php71w-pdo php71w-zip
+sudo yum -y install php71w php71w-curl php71w-common php71w-cli php71w-mysql php71w-mbstring php71w-fpm php71w-xml php71w-pdo php71w-zip
+
 sudo systemctl enable php-fpm
 sudo systemctl start php-fpm
 
